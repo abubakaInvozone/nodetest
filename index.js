0bc5573255b9a72c969495c26788eb58e34f81c2
abubakar.waqas <abubakar.waqas@invozone.com>
2022-04-22 00:48:10 +0500

    fix changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send(`<h1>Hello Node JS is running!</h1>`);
+  res.send(`<h3>Hello Node JS! </h3>`);
 });
 
 app.listen(3000, function () {
