9128dc30cd797dd08135894e88f8e2de35b9d365
abubakar.waqas <abubakar.waqas@invozone.com>
2022-09-16 22:46:30 +0500

    configiure backend successfully

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send(`<h1>Hello Root endpoint of NodeJS => 
-  (EC2 configure with NGINX port 2000) </h1>`);
+  res.send(`<h1>Congratuulation successfullty confugure Webhooks </h1>`);
 });
 
 app.listen(3000, function () {
