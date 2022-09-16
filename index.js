c4ff59a93c3d74e6aa8c051e033ce27f46cefcd5
abubakar.waqas <abubakar.waqas@invozone.com>
2022-09-16 15:09:52 +0500

    typo issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get("/", function (req, res) {
   res.send(`<h1>Hello Root endpoint of NodeJS => 
-  (EC2 configure with NGINX port) </h1>`);
+  (EC2 configure with NGINX port 2000) </h1>`);
 });
 
 app.listen(3000, function () {
