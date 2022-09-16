a84dd884c8b124d80192daf2d5c89eef2b22b08c
abubakar.waqas <abubakar.waqas@invozone.com>
2022-09-16 17:14:49 +0500

    githooks update

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,8 +4,9 @@
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/ec2-user/express-app
 
+
 #install node modules
 npm install
 
 #start our node app in the background
-npm start&
+node app.js > app.out.log 2> app.err.log < /dev/null & 
