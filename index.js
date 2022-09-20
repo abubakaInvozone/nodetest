672cb561810d75fff785c46e064e08f67ebf55f9
abubakar.waqas <abubakar.waqas@invozone.com>
2022-09-20 11:54:42 +0500

    server down

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send(
-    `<h1>Congratuulation Successfullty Confugure Webhooks (noted) </h1>`
-  );
+  res.send(`<h1>Congratuulation Successfullty Confugure Webhook(noted) </h1>`);
 });
 
 app.listen(3000, function () {
